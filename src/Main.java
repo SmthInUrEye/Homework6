f450891944ef332ecb088d41e74c160a6add3654
SmthInUrEye <dragon-pg@yandex.ru>
2024-09-28 21:58:05 +0300

    Update src/Main.java
    Co-authored-by: Ilya Savinov <il.savinov@gmail.com>

--- src/Main.java
+++ src/Main.java
@@ -14,7 +14,7 @@ public class Main {
         }
 
         System.out.println ( "\nЗадача №3" );
-        for (int i = 0; i <= 17; i = i + 2) {
+        for (int i = 0; i <= 17; i +=  2) {
             System.out.print ( " " + i );
         }
 
